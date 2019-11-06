88298098bbf9758a1f571e97a40d05f2641f4ba8
Arturo Wagener <awagener@minsait.com>
2019-11-06 09:02:35 +0100

    Room Creation Almost fixed (TODO: Control the generation rand with some ranges having in mind the number of rooms left to create) Room Position Done. Room Rotation Not Done.

--- Assets/DungeonGenerator_Controller.cs
+++ Assets/DungeonGenerator_Controller.cs
@@ -37,6 +37,7 @@ public class DungeonGenerator_Controller : MonoBehaviour
     [Tooltip("Size of the dungeon in squares^2")]
     [SerializeField]
     int dungeon_size;
+    int currentParentRoom;
 
     private void Start()
     {
@@ -46,133 +47,76 @@ public class DungeonGenerator_Controller : MonoBehaviour
     void GenerateDungeon()
     {
         int length = dungeon_size;
-        int rand;
+        
 
         GameObject[] tempRooms = new GameObject[length];
         DungeonGenerator_Room[] tempRoomScript = new DungeonGenerator_Room[length];
-        int currentParentRoom;
+
         
 
-        tempRooms[0] = Instantiate(NewRoom(3));
-        tempRoomScript[0] = tempRooms[0].GetComponent<DungeonGenerator_Room>();
-        currentParentRoom = 0;
-
         Debug.Log(length);
-        for (int i = 1; i < length; i++)
+        for (int i = 0; i < length; i++)
         {
-            if (i > length-4)
+            if (i == 0)
+            {
+                tempRooms[0] = Instantiate(NewRoom(3));
+                tempRoomScript[0] = tempRooms[0].GetComponent<DungeonGenerator_Room>();
+                currentParentRoom = 0;
+            }else if (i > length-4)
             {
                 if (i > length - 3)
                 {
                     if (i > length - 2)
                     {
-                        int doorsLength = tempRoomScript[currentParentRoom].GetDoorsCount();
-                        for (int d = 0; d < doorsLength; d++)
-                        {
-                            if (tempRoomScript[currentParentRoom].IsConnected(d) == false)
-                            {
-                                rand = 0;
-                                tempRooms[i + d] = Instantiate(NewRoom(rand));
-                                tempRoomScript[i + d] = tempRooms[i+d].GetComponent<DungeonGenerator_Room>();
-
-                                //Colocar la room
-                                Vector3 posRelCurrentRoom;
-                                Vector3 posRelInstRoom;
-                                Vector3 dirCurrentRoom;
-                                Vector3 dirInstantiateRoom;
-
-
-
-
-                                tempRoomScript[i + d].ConnectDoor(0);
-                            }
-                        }
-
-                        currentParentRoom++;
+                        GenerateRooms(tempRoomScript, tempRooms, i, 0);
                     }
                     else
                     {
-                        int doorsLength = tempRoomScript[currentParentRoom].GetDoorsCount();
-                        for (int d = 0; d < doorsLength; d++)
-                        {
-                            if (tempRoomScript[currentParentRoom].IsConnected(d) == false)
-                            {
-                                rand = 1;
-                                tempRooms[i + d] = Instantiate(NewRoom(rand));
-                                tempRoomScript[i + d] = tempRooms[i+d].GetComponent<DungeonGenerator_Room>();
-
-                                //Colocar la room
-                                Vector3 posRelCurrentRoom;
-                                Vector3 posRelInstRoom;
-                                Vector3 dirCurrentRoom;
-                                Vector3 dirInstantiateRoom;
-
-
-
-
-                                tempRoomScript[i + d].ConnectDoor(0);
-                            }
-                        }
-
-                        currentParentRoom++;
+                        GenerateRooms(tempRoomScript, tempRooms, i, 1);
                     }
                 }
                 else
                 {
-                    int doorsLength = tempRoomScript[currentParentRoom].GetDoorsCount();
-                    for (int d = 0; d < doorsLength; d++)
-                    {
-                        if (tempRoomScript[currentParentRoom].IsConnected(d) == false)
-                        {
-                            rand = 0;
-                            tempRooms[i + d] = Instantiate(NewRoom(rand));
-                            tempRoomScript[i + d] = tempRooms[i+d].GetComponent<DungeonGenerator_Room>();
-
-                            //Colocar la room
-                            Vector3 posRelCurrentRoom;
-                            Vector3 posRelInstRoom;
-                            Vector3 dirCurrentRoom;
-                            Vector3 dirInstantiateRoom;
-
-
-
-
-                            tempRoomScript[i + d].ConnectDoor(0);
-                        }
-                    }
-
-                    currentParentRoom++;
+                    GenerateRooms(tempRoomScript, tempRooms, i, 2);
                 }
             }
             else
             {
-                int doorsLength = tempRoomScript[currentParentRoom].GetDoorsCount();
-                for (int d = 0; d < doorsLength; d++)
-                {
-                    if (tempRoomScript[currentParentRoom].IsConnected(d) == false)
-                    {
-                        rand = Random.Range(0, 3);
-                        tempRooms[i+d] = Instantiate(NewRoom(rand));
-                        tempRoomScript[i+d] = tempRooms[i+d].GetComponent<DungeonGenerator_Room>();
-
-                        //Colocar la room
-                        Vector3 posRelCurrentRoom;
-                        Vector3 posRelInstRoom;
-                        Vector3 dirCurrentRoom;
-                        Vector3 dirInstantiateRoom;
-
-                        
-
-
-                        tempRoomScript[i+d].ConnectDoor(0);
-                    }
-                }
-
-                currentParentRoom++;
-
+                GenerateRooms(tempRoomScript, tempRooms, i, 3);
             }
-            Debug.Log(i);
         }
+    }
+
+    void GenerateRooms(DungeonGenerator_Room[] _tempRoomScript, GameObject[] _tempRooms, int i, int randNum)
+    {
+        int rand;
+
+        int doorsLength = _tempRoomScript[currentParentRoom].GetDoorsCount();
+        Debug.Log("Door num: " + currentParentRoom + " has " + doorsLength + " doors.");
+        for (int d = 0; d < doorsLength; d++)
+        {
+            if (_tempRoomScript[currentParentRoom].IsConnected(d) == false)
+            {
+                rand = Random.Range(0, randNum+1);
+                if (rand == randNum+1)
+                {
+                    rand = 0;
+                }
+                _tempRooms[i + d] = Instantiate(NewRoom(rand));
+                _tempRoomScript[i + d] = _tempRooms[i + d].GetComponent<DungeonGenerator_Room>();
+
+                //Colocar la room
+                Debug.Log("Adding Room to Door " + d);
+                NewRoomTransf(_tempRooms[currentParentRoom], d, _tempRooms[i + d]);
+
+
+
+
+
+                _tempRoomScript[i + d].ConnectDoor(0);
+            }
+        }
+        currentParentRoom++;
     }
 
     GameObject NewRoom(int i)
@@ -181,22 +125,46 @@ public class DungeonGenerator_Controller : MonoBehaviour
         switch (i)
         {
             case 0:
-                randomRoom = Random.Range(0, one_prefabs.Length);
+                randomRoom = Random.Range(0, one_prefabs.Length-1);
                 return one_prefabs[randomRoom];
             case 1:
-                randomRoom = Random.Range(0, two_prefabs.Length);
+                randomRoom = Random.Range(0, two_prefabs.Length-1);
                 return two_prefabs[randomRoom];
             case 2:
-                randomRoom = Random.Range(0, three_prefabs.Length);
+                randomRoom = Random.Range(0, three_prefabs.Length-1);
                 return three_prefabs[randomRoom];
             case 3:
-                randomRoom = Random.Range(0, four_prefabs.Length);
+                randomRoom = Random.Range(0, four_prefabs.Length-1);
                 return four_prefabs[randomRoom];
             default:
-                randomRoom = Random.Range(0, one_prefabs.Length);
+                randomRoom = Random.Range(0, one_prefabs.Length-1);
                 return one_prefabs[randomRoom];
         }
         
     }
+
+    void NewRoomTransf(GameObject c_Room,int c_doorNum, GameObject n_Room)
+    {
+        
+        Transform c_door = c_Room.GetComponent<DungeonGenerator_Room>().GetDoor(c_doorNum);
+        Transform n_door = n_Room.GetComponent<DungeonGenerator_Room>().GetDoor(0);
+
+        //Position
+        Vector3 doorPos = c_door.position;
+        n_Room.GetComponent<DungeonGenerator_Room>().ConnectDoor(0);
+
+        Vector3 finalPos = doorPos + n_door.localPosition;
+
+        //Rotation
+        Quaternion c_doorRot = c_door.rotation;
+
+        Vector3 eulerRot = c_doorRot.eulerAngles + Quaternion.Inverse(c_doorRot).eulerAngles;
+
+        Quaternion finalRot = Quaternion.Euler(eulerRot);
+
+        n_Room.transform.position = finalPos;
+        n_Room.transform.rotation = finalRot; 
+    }
+    
 
 }
