32cadad8313f762437178c3aca5aad8512f9483d
Takeo7 <arthur_wag@hotmail.com>
2022-10-17 17:35:17 +0200

    Jump
    Starting the including of the jump feature

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -19,9 +19,15 @@ public class PlayerMovement : MonoBehaviour
 
     [Space]
     [SerializeField]
-    float speed;
+    float walkSpeed;
+    [SerializeField]
+    float runSpeed;
     [SerializeField]
     bool IsWalking = false;
+    [SerializeField]
+    bool IsRunning = false;
+    [SerializeField]
+    bool IsJumping = false;
     [SerializeField]
     float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
@@ -36,23 +42,55 @@ public class PlayerMovement : MonoBehaviour
 
         if (direction.magnitude >= 0.1f)
         {
-            if (IsWalking == false)
+            if (Input.GetKey(KeyCode.LeftShift))
             {
-                IsWalking = !IsWalking;
-                anim.SetBool("IsWalking", IsWalking);
-            }
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y , targetAngle, ref turnSmoothVelocity, turnSmoothTime);
-            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+                if (IsRunning == false)
+                {
+                    IsRunning = !IsRunning;
+                    anim.SetBool("IsRunning", IsRunning);
 
-            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            Debug.Log("moveDir: " + moveDir + "\nnovedir.normalized: " + moveDir.normalized);
-            nma.Move(moveDir.normalized * speed * Time.deltaTime);
+                }
+
+                float targetAngle_r = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+                float angle_r = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle_r, ref turnSmoothVelocity, turnSmoothTime);
+                transform.rotation = Quaternion.Euler(0f, angle_r, 0f);
+
+                Vector3 moveDir_r = Quaternion.Euler(0f, targetAngle_r, 0f) * Vector3.forward;
+                Debug.Log("runDir: " + moveDir_r + "\nrundir.normalized: " + moveDir_r.normalized);
+                nma.Move(moveDir_r.normalized * runSpeed * Time.deltaTime);
+            }
+            else
+            {
+                if (IsWalking == false)
+                {
+                    IsWalking = !IsWalking;
+                    anim.SetBool("IsWalking", IsWalking);
+                }
+                float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+                float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+                transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+                Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+                Debug.Log("moveDir: " + moveDir + "\nnovedir.normalized: " + moveDir.normalized);
+                nma.Move(moveDir.normalized * walkSpeed * Time.deltaTime);
+            }
+            
         }
         else if (IsWalking == true)
         {
             IsWalking = !IsWalking;
             anim.SetBool("IsWalking", IsWalking);
         }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            IsRunning = !IsRunning;
+            anim.SetBool("IsRunning", IsRunning);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetTrigger("IsJumping");
+        }
+
+
     }
 }
